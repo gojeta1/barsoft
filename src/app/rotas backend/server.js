20ba8076fa4573a99bb44761815d66f5d24278e0
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-01 21:03:35 -0300

    Update server.js

--- src/app/rotas backend/server.js
+++ src/app/rotas backend/server.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
-
+const users = require('./users.json');
+const cors = require('cors');
+  
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000/login'
+    origin: 'http://localhost:4200'
   }));
 
-app.use(bodyParser.json());
 
-const secretKey = 'my-secret-key';
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const users = require('./users.json');
-  const user = users.users.find(u => u.username === username && u.password === password );
-
-  if (!user) {
-    return res.status(401).json({ message: 'Usuário ou senha inválidos' });
+function generateToken(user) {
+    const token = jwt.sign({ id: user.id }, secret, { expiresIn: 86400 });
+    return token;
   }
 
-  const token = jwt.sign({ username }, secretKey);
+function verifyPassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
 
-  return res.status(200).json({ token });
-});
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+    if (!user || !verifyPassword(password, user.password)) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = generateToken(user);
+    return res.json({ token });
+  })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+// Rota protegida que só pode ser acessada com um token de autenticação válido
+app.get('/protected', (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ message: 'Missing authorization header' });
+    }
+    try {
+      const decoded = jwt.verify(token, secret);
+      const user = users.find(u => u.id === decoded.id);
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      return res.json({ message: 'Hello, ' + user.username + '!' });
+    } catch (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  });
