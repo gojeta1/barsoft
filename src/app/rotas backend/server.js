7bf2adb664beb146fdfb2f696d817b87085010e4
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-08 21:26:56 -0300

    Update server.js

--- src/app/rotas backend/server.js
+++ src/app/rotas backend/server.js
@@ -1,27 +1,46 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const users = require('./users.json');
-
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
+// Configurando o body-parser
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const JWT_SECRET = 'my-secret';
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  const user = users.users.find((u) => u.username === username && u.password === password);
-  console.log (users)
-  if (user) {
-    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' });
-
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: 'Usuário ou senha inválidos' });
-  }
+// Configurando a conexão com o banco de dados
+const connection = mysql.createConnection({
+  host: 'database-1.c1y1f7ntndzm.sa-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Chuva281180',
+  database: 'dbsoft'
 });
 
-app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
+// Conectando ao banco de dados
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Conectado ao banco de dados MySQL!');
+});
 
+// Criando a rota de login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  // Consultando o banco de dados
+  connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(err, results, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erro interno no servidor.');
+    } else if (results.length > 0) {
+      res.status(200);
+      res.json({ success: true, message: 'Login Realizado com sucesso'});
+    } else {
+      res.status(401).send('Usuário ou senha incorretos.');
+    }
+  });
+});
+
+// Iniciando o servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000!');
+});
