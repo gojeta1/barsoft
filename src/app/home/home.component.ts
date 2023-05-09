9ec1e762124673980e22a871b012d32b2d17a59f
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-08 21:26:53 -0300

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  constructor(private http: HttpClient) {}
+
+  
 
 }
