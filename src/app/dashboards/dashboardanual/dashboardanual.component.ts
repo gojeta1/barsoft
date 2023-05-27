d3096d36c6646f88921c4c1c80f3a391eeb0431b
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-27 18:37:47 -0300

    Create dashboardanual.component.ts

---
+++ src/app/dashboards/dashboardanual/dashboardanual.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardanual',
+  templateUrl: './dashboardanual.component.html',
+  styleUrls: ['./dashboardanual.component.scss']
+})
+export class DashboardanualComponent {
+
+}
