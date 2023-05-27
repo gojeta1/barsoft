f8cca6ffd49230883b796c04f14f7573b80dded2
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-27 18:37:23 -0300

    Create dashboardmensal.component.ts

---
+++ src/app/dashboards/dashboardmensal/dashboardmensal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardmensal',
+  templateUrl: './dashboardmensal.component.html',
+  styleUrls: ['./dashboardmensal.component.scss']
+})
+export class DashboardmensalComponent {
+
+}
