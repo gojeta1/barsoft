0bddc5fc1ca713a51438500fd3c9d14145a05b37
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-27 18:37:35 -0300

    Create dashboardgeral.component.ts

---
+++ src/app/dashboards/dashboardgeral/dashboardgeral.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardgeral',
+  templateUrl: './dashboardgeral.component.html',
+  styleUrls: ['./dashboardgeral.component.scss']
+})
+export class DashboardgeralComponent {
+
+}
