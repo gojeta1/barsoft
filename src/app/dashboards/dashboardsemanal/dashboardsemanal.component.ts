2e5135981bda4e177f5f307e4fca10a34a4e745c
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-27 18:37:13 -0300

    Create dashboardsemanal.component.ts

---
+++ src/app/dashboards/dashboardsemanal/dashboardsemanal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardsemanal',
+  templateUrl: './dashboardsemanal.component.html',
+  styleUrls: ['./dashboardsemanal.component.scss']
+})
+export class DashboardsemanalComponent {
+
+}
