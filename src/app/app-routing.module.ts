ccf6cc127f93a3c9a0ca1155074d8e7974e9d8fd
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-29 17:51:16 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
-import { AuthenticationService } from './authentication.service';
 import { CadastroclientesComponent } from './cadastros/clientes/cadastroclientes/cadastroclientes.component';
+import { DashboardgeralComponent } from './dashboards/dashboardgeral/dashboardgeral.component';
+import { DashboardsemanalComponent } from './dashboards/dashboardsemanal/dashboardsemanal.component';
+import { DashboardmensalComponent } from './dashboards/dashboardmensal/dashboardmensal.component';
+import { DashboardanualComponent } from './dashboards/dashboardanual/dashboardanual.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent, canActivate:[AuthGuard], children:[
-    { path: 'cadastroclientes', component: CadastroclientesComponent, outlet:'main'}
+    { path: 'cadastroclientes', component: CadastroclientesComponent, outlet:'main'},
+    { path: 'dashboardgeral', component: DashboardgeralComponent, outlet:'main'},
+    { path: 'dashboardsemanal', component: DashboardsemanalComponent, outlet:'main'},
+    { path: 'dashboardmensal', component: DashboardmensalComponent, outlet:'main'},
+    { path: 'dashboardanual', component: DashboardanualComponent, outlet:'main'}
   ]},
   { path: 'cadastroclientes', component: CadastroclientesComponent, canActivate:[AuthGuard]}
 ];
