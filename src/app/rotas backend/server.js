93960dfe807e587598b816a8f0847a85a46d8822
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-05-31 02:03:07 -0300

    Update server.js

--- src/app/rotas backend/server.js
+++ src/app/rotas backend/server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const multer = require('multer');
+const path = require('path');
 
 app.use(cors());
 // Configurando o body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('/uploads'));
 
 // Configurando a conexão com o banco de dados
 const connection = mysql.createConnection({
@@ -24,22 +26,51 @@ connection.connect(function(err) {
   console.log('Conectado ao banco de dados MySQL!');
 });
 
+
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const extension = path.extname(file.originalname);
+    cb(null, Date.now() + extension);
   }
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  const allowedExtensions = ['.jpg', '.jpeg', '.png'];
+  const extension = path.extname(file.originalname);
+  if (allowedExtensions.includes(extension.toLowerCase())) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file extension'));
+  }
+};
 
-app.post('/uploadProfilePicture', upload.single('profilePicture'), (req, res) => {
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+app.put('/uploadProfilePicture', upload.single('profilePicture'), (req, res) => {
   // Aqui você pode receber e processar a imagem enviada pelo cliente e armazenar no banco de dados
   // Certifique-se de configurar a pasta 'uploads/' para salvar as imagens
-  console.log('Imagem recebida:', req.file);
-  res.sendStatus(200);
+  const profileImage = req.file.filename;
+  const imageUrl = 'http://localhost:3000/uploads/' + profileImage;
+  const usuarioId = req.params.usuarioId;
+  const novaFoto = req.body.foto;
+  
+
+  const query = 'UPDATE users SET foto = ? WHERE id = ?';
+  connection.query(query, [novaFoto, usuarioId], (err, result) => {
+    if (err) {
+      console.error('Erro ao salvar a imagem no banco de dados: ' + err.stack);
+      res.status(500).json({ message: 'Erro ao salvar a imagem no banco de dados' });
+      return;
+    }
+    
+    res.json({ message: 'Upload realizado com sucesso!', imageUrl: imageUrl });
+        // Fechar a conexão com o banco de dados
+    connection.end();
+  });
 });
 
 // Criando a rota de login
@@ -65,6 +96,7 @@ app.post('/login', (req, res) => {
       res.json({ success: true,  message: 'Login Realizado com sucesso', nomeUsuario: nomeUsuario});
     } else {
       res.status(401).json({ message: 'Usuário ou senha incorretos' });
+      connection.end();
     }
   });
 });
@@ -99,6 +131,7 @@ app.post('/cadclientes', (req, res) => {
     // Neste exemplo, vamos verificar se o e-mail contém um @
     return email.includes('@');
   }
+  connection.end();
 });
 
 app.post('/cadusuario', (req, res) => {
@@ -117,8 +150,8 @@ app.post('/cadusuario', (req, res) => {
     res.status(200)
     console.log('usuario cadastrado com sucesso')
     return res.json({message: 'Usuário cadastrado com sucesso'});
-  })
-})
+  });
+});
 
 // Iniciando o servidor
 app.listen(3000, () => {
