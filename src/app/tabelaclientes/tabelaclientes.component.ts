f9387e0c03c054d8b8e0a48c49663d7835c6a664
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-06-02 13:46:01 -0300

    Create tabelaclientes.component.ts

---
+++ src/app/tabelaclientes/tabelaclientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabelaclientes',
+  templateUrl: './tabelaclientes.component.html',
+  styleUrls: ['./tabelaclientes.component.scss']
+})
+export class TabelaclientesComponent {
+
+}
