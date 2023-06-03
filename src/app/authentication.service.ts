76ba09373d30b8e5a88a184ea7e9f1faf57890e5
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-06-03 18:36:32 -0300

    Update authentication.service.ts

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {  Router } from '@angular/router';
 import { NotificationService } from './notification.service';
-import { BehaviorSubject } from 'rxjs';
-
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,26 +15,30 @@ export class AuthenticationService {
   aguardandoResposta = true;
   carregando = true;
   private isAuthenticated = false;
-  nomeUser: any;
-  userId: any;
+
   
   constructor(private http: HttpClient, private router: Router, private notificationService: NotificationService) { }
   
 
-  login(username: string, password: string): void{
-
+  login(username: string, password: string){
+     
      this.http.post<any>(`${this.apiUrl}/login`, {username, password}).subscribe(
       (response) => {
         console.log(response.message);
 
         // Faça o que for necessário após o login bem-sucedido
         if(response.success === true){
-          this.nomeUser = response.nomeUsuario;
-          this.userId = response.userId;
-          console.log(this.userId)
+
+          const token = response.token;
+          const userId = response.userId;
+          const nomeUser = response.nomeUsuario;
+          localStorage.setItem('token', token);
+          localStorage.setItem('userId', userId);
+          localStorage.setItem('nomeUser', nomeUser);
           this.router.navigate(['/home'])
           this.isAuthenticated = true;
           return;
+          
           }
       },
       (error) => {
@@ -47,19 +50,18 @@ export class AuthenticationService {
     
   }
 
-  getNomeUser(){
-    return this.nomeUser;
-  }
-
-  getUserId(){
-    return this.userId;
-  }
-
   logout(): void {
     // Lógica de logout
+    localStorage.removeItem('token');
     // Realize as ações necessárias para deslogar o usuário, como limpar o token de autenticação, remover informações da sessão, etc.
-
     this.isAuthenticated = false;
+    this.router.navigate(['/login']);
+  }
+
+  checkAuthentication(): void {
+    const token = localStorage.getItem('token');
+    this.isAuthenticated = !!token; // Verifica se o token existe
+    
   }
 
   isAuthenticatedUser(): boolean {
