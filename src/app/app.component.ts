e9a4919227dba92018d1b5c00287de7f91c01f61
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-06-03 18:36:28 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 
 
@@ -7,6 +7,11 @@ import { AuthenticationService } from './authentication.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  constructor(private authenticationService: AuthenticationService){}
+
+  ngOnInit(): void {
+    this.authenticationService.checkAuthentication();
+  }
 }
