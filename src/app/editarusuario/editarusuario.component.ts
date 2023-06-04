2db4be80088a7782b38bb9a851e2aeff52598053
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2023-06-04 17:12:54 -0300

    Update editarusuario.component.ts

--- src/app/editarusuario/editarusuario.component.ts
+++ src/app/editarusuario/editarusuario.component.ts
@@ -25,6 +25,12 @@ export class EditarUsuarioComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.userId = localStorage.getItem('userId')
+    const storedProfileImage = localStorage.getItem('profileImagem');
+  if (!storedProfileImage || storedProfileImage === '') {
+    this.profileImage = './assets/jhon/padrao.png';
+  } else {
+    this.profileImage = storedProfileImage;
+  }
     this.getProfileImage();
     this.cdr.detectChanges();
   }
@@ -32,12 +38,35 @@ export class EditarUsuarioComponent implements OnInit, AfterViewInit {
 
   getProfileImage() {
     const timestamp = new Date().getTime();
-    const imageUrl = `http://localhost:3000/users/${this.userId}/profileImage?timestamp=${timestamp}`;
-    
+    let imageUrl = `http://localhost:3000/users/${this.userId}/profileImage?timestamp=${timestamp}`;
 
+    console.log('Valor de this.profileImage:', this.profileImage);
+
+    if (!this.profileImage || this.profileImage.includes('undefined')) {
+      // Consultar o banco de dados para obter o caminho da foto de perfil
+      this.http.get<any>(`http://localhost:3000/users/${this.userId}`).subscribe(
+        (response) => {
+          if (response && response.profile_image) {
+            // Caso tenha um caminho de imagem no banco de dados
+            this.profileImage = response.profile_image;
+            localStorage.clear();
+          } else {
+            // Caso não tenha um caminho de imagem no banco de dados, usar imagem padrão
+            this.profileImage = './assets/jhon/padrao.png';
+          }
+        },
+        (error) => {
+          console.error('Erro ao obter a foto de perfil do usuário:', error);
+          // Caso ocorra algum erro, usar imagem padrão
+          this.profileImage = './assets/jhon/padrao.png';
+        }
+      );
+    }
+  
     this.profileImage = imageUrl; // Atribui novamente a URL com o timestamp atualizado
     localStorage.setItem('userId', this.userId);
     localStorage.setItem('profileImagem', this.profileImage);
+    console.log('Valor de this.profileImage após atribuição:', this.profileImage);
   }
 
   getCurrentTimestamp() {
